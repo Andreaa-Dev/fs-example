f4d74d1fe7f2cb33e1ee266256a30d083a45efcd
Andrea <ngocanhhsgs@gmail.com>
2023-03-21 23:47:41 +0200

    add tst

---
+++ api/test/sum.test.ts
@@ -0,0 +1,20 @@
+import { sumNumbers } from "../src/sum/sum";
+
+describe("sum function", () => {
+  // Define a test case using the test function
+  test("adds 1 + 2 to equal 3", () => {
+    // Call the sum function with arguments
+    const result = sumNumbers(1, 2);
+
+    // Use the expect function to make an assertion
+    expect(result).toBe(3);
+  });
+
+  // Define another test case
+  test("adds 0 + 0 to equal 0", () => {
+    const result = sumNumbers(0, 0);
+    expect(result).toBe(0);
+  });
+
+  // Add more test cases as needed
+});
